f5b2c9af3f6bbdf5220e46aa21cb9e2390f8081e
FK-Phoenix <1044013812@qq.com>
2019-06-25 14:45:39 +0800

    Merge branch 'master' of https://github.com/FK-Phoenix/his_front
    # Conflicts:
    #	src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,12 @@
             <el-menu-item index="1">处理中心</el-menu-item>
             <el-submenu index="2">
               <template slot="title">Management</template>
-              <el-menu-item index="2-1" @click="naviTo('dept')">Departments</el-menu-item>
-              <el-menu-item index="2-2">Diagnostic Catalog</el-menu-item>
-              <el-menu-item index="2-3">Non-Medical Charge</el-menu-item>
-              <el-menu-item index="2-4">Shift</el-menu-item>
-              <el-menu-item index="2-5">Users</el-menu-item>
+              <el-menu-item index="2-1">Constant</el-menu-item>
+              <el-menu-item index="2-2" @click="naviTo('dept')">Departments</el-menu-item>
+              <el-menu-item index="2-3">Diagnostic Catalog</el-menu-item>
+              <el-menu-item index="2-4">Non-Medical Charge</el-menu-item>
+              <el-menu-item index="2-5">Shift</el-menu-item>
+              <el-menu-item index="2-6">Users</el-menu-item>
             </el-submenu>
             <el-submenu index="3">
               <template slot="title">Register&Payment</template>
