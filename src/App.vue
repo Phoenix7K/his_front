b5730d9bbe5731a7c9a89a72f463a63d6d7d2846
Ka12un <928330629@qq.com>
2019-06-25 14:20:33 +0800

    New Start

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,73 @@
 <template>
   <div id="app">
-    <el-row style="height: 100px">
-      <el-col :span="4">
-        <el-image
-          style="width: 100%  ; height: 100%"
-          src="./assets/logo.png"
-        ></el-image>
-      </el-col>
-      <el-col :span="20">
-        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-          <el-menu-item index="1">处理中心</el-menu-item>
-          <el-submenu index="2">
-            <template slot="title">Management</template>
-            <el-menu-item index="2-1">Departments</el-menu-item>
-            <el-menu-item index="2-2">Diagnostic Catalog</el-menu-item>
-            <el-menu-item index="2-3">Non-Medical Charge</el-menu-item>
-            <el-menu-item index="2-4">Shift</el-menu-item>
-            <el-menu-item index="2-5">Users</el-menu-item>
-          </el-submenu>
-          <el-submenu index="3">
-            <template slot="title">Register&Payment</template>
-            <el-menu-item index="3-1">Register</el-menu-item>
-            <el-menu-item index="3-2">Receipt</el-menu-item>
-            <el-menu-item index="3-3">Bill</el-menu-item>
-          </el-submenu>
-          <el-submenu index="4">
-            <template slot="title">Outpatient</template>
-            <el-menu-item index="4-1">Homepage</el-menu-item>
-            <el-menu-item index="4-2">Template Management</el-menu-item>
-          </el-submenu>
-          <el-submenu index="5">
-            <template slot="title">Medical Treatment</template>
-            <el-menu-item index="5-1">Examination</el-menu-item>
-            <el-menu-item index="5-2">Medical Management</el-menu-item>
-          </el-submenu>
-          <el-submenu index="6">
-            <template slot="title">Pharmacy</template>
-            <el-menu-item index="6-1">Send Medicine</el-menu-item>
-            <el-menu-item index="6-2">Return Medicine</el-menu-item>
-            <el-menu-item index="6-3">Medicine Management</el-menu-item>
-          </el-submenu>
-          <el-submenu index="7">
-            <template slot="title">Financial</template>
-            <el-menu-item index="7-1">Daily Record</el-menu-item>
-            <el-menu-item index="7-2">Departments</el-menu-item>
-            <el-menu-item index="7-3">Doctors</el-menu-item>
-          </el-submenu>
-          <el-menu-item>
-            <el-row type="flex" justify="space-between">
-              <el-col :span="10">
-                <el-tag style="font-family: 'Microsoft Sans Serif';" type="primary">
-                  {{this.usercat}}:{{this.username}}
-                </el-tag>
-              </el-col>
-              <el-col :span="12" style="padding:0px">
-                <i class="el-icon-s-custom"></i>
-                <el-button v-bind:type="loginButton" size="small" @click="loginDialog">{{loginState}}</el-button>
-              </el-col>
-            </el-row>
-          </el-menu-item>
-        </el-menu>
-      </el-col>
-    </el-row>
-
+    <el-header>
+      <el-row style="height: 100px">
+        <el-col :span="24">
+          <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+            <el-menu-item index="1">处理中心</el-menu-item>
+            <el-submenu index="2">
+              <template slot="title">Management</template>
+              <el-menu-item index="2-1" @click="naviTo('dept')">Departments</el-menu-item>
+              <el-menu-item index="2-2">Diagnostic Catalog</el-menu-item>
+              <el-menu-item index="2-3">Non-Medical Charge</el-menu-item>
+              <el-menu-item index="2-4">Shift</el-menu-item>
+              <el-menu-item index="2-5">Users</el-menu-item>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title">Register&Payment</template>
+              <el-menu-item index="3-1">Register</el-menu-item>
+              <el-menu-item index="3-2">Receipt</el-menu-item>
+              <el-menu-item index="3-3">Bill</el-menu-item>
+            </el-submenu>
+            <el-submenu index="4">
+              <template slot="title">Outpatient</template>
+              <el-menu-item index="4-1">Homepage</el-menu-item>
+              <el-menu-item index="4-2">Template Management</el-menu-item>
+            </el-submenu>
+            <el-submenu index="5">
+              <template slot="title">Medical Treatment</template>
+              <el-menu-item index="5-1">Examination</el-menu-item>
+              <el-menu-item index="5-2">Medical Management</el-menu-item>
+            </el-submenu>
+            <el-submenu index="6">
+              <template slot="title">Pharmacy</template>
+              <el-menu-item index="6-1">Send Medicine</el-menu-item>
+              <el-menu-item index="6-2">Return Medicine</el-menu-item>
+              <el-menu-item index="6-3">Medicine Management</el-menu-item>
+            </el-submenu>
+            <el-submenu index="7">
+              <template slot="title">Financial</template>
+              <el-menu-item index="7-1">Daily Record</el-menu-item>
+              <el-menu-item index="7-2">Departments</el-menu-item>
+              <el-menu-item index="7-3">Doctors</el-menu-item>
+            </el-submenu>
+            <el-menu-item style="float: right;margin-right: 30px">
+              <el-row type="flex" justify="space-between">
+                <el-col :span="10">
+                  <el-tag style="font-family: 'Microsoft Sans Serif';" type="primary">
+                    {{this.usercat}}:{{this.username}}
+                  </el-tag>
+                </el-col>
+                <el-col :span="12" style="padding:0px">
+                  <i class="el-icon-s-custom"></i>
+                  <el-button v-bind:type="loginButton" size="small" @click="">{{loginState}}</el-button>
+                </el-col>
+              </el-row>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-main>
+      <router-view></router-view>
+    </el-main>
+    <el-footer>
+      <el-divider><i class="el-icon-mobile-phone"></i></el-divider>
+      <el-row justify="space-around">
+        <el-col :span="12">NEUEDU SOFT</el-col>
+        <el-col :span="12">MaxSwen: 10086</el-col>
+      </el-row>
+    </el-footer>
   </div>
 </template>
 
@@ -81,6 +86,9 @@
     methods: {
       handleSelect(key, keyPath) {
         console.log(key, keyPath);
+      },
+      naviTo(url){
+        this.$router.push({path:'/'+url})
       }
     }
   }
---
+++ src/components/dept.vue
@@ -0,0 +1,291 @@
+<template>
+  <div id="department">
+
+    <el-row :gutter="20">
+      <el-col :span="4">
+        <el-input type="text" v-model="deptcode" placeholder="Code" clearable></el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-input type="text" v-model="deptclass" placeholder="Class" clearable></el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-input type="text" v-model="deptname" placeholder="Name" clearable></el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-input type="text" v-model="deptcat" placeholder="Category" clearable></el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-button icon="el-icon-search" type="primary" @click="search">Search</el-button>
+      </el-col>
+      <el-col :span="4">
+        <el-button icon="el-icon-circle-plus-outline" type="primary" @click="showDialogForm('Add')">Add
+        </el-button>
+      </el-col>
+    </el-row>
+
+
+    <template>
+      <el-table :data="depts.slice((currentPage-1)*pagesize,currentPage*pagesize)" style="width: 100%">
+        <el-table-column prop="deptcode" label="Code" width="180">
+        </el-table-column>
+        <el-table-column prop="deptclass" label="Class" width="180">
+        </el-table-column>
+        <el-table-column prop="deptname" label="Name" width="180">
+        </el-table-column>
+        <el-table-column prop="deptcat" label="Category">
+        </el-table-column>
+        <el-table-column fixed="right" label="Operations" width="120">
+          <template slot-scope="scope">
+            <el-button @click="showDeleteDialogForm(scope.$index, scope.row)" type="text" size="small">
+              delete
+            </el-button>
+            <el-button @click="showDialogForm('Update',scope.$index, scope.row)" type="text" size="small">
+              update
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage"
+                     :page-sizes="[5, 10, 20, 40]" :page-size="pagesize" :hide-on-single-page="value"
+                     layout="total, sizes, prev, pager, next, jumper" :total="depts.length">
+      </el-pagination>
+
+    </template>
+
+    <el-dialog :title="formTitle" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item v-if="methodType=='Add'" label="Code" :label-width="formLabelWidth">
+          <el-input v-model="form.deptcode" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Class" :label-width="formLabelWidth">
+          <el-input v-model="form.deptclass" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Name" :label-width="formLabelWidth">
+          <el-input v-model="form.deptname" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Category" :label-width="formLabelWidth">
+          <el-input v-model="form.deptcat" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="onSubmit">{{methodType}}</el-button>
+      </div>
+    </el-dialog>
+
+    <!--    Delete Dialogue -->
+    <el-dialog title="Confirm Delete" :visible.sync="deleteDialogVisible" width="30%">
+      <span>Are you sure you want to delete this?</span>
+      <span slot="footer" class="dialog-footer">
+                <el-button @click="deleteDialogVisible = false">Cancel</el-button>
+                <el-button type="primary" @click="onSubmitDelete">Confirm</el-button>
+            </span>
+    </el-dialog>
+
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "department",
+    data() {
+      return {
+        deptcode: "",
+        deptclass: "",
+        deptname: "",
+        deptcat: "",
+        depts: [],
+
+        value: false,
+        currentPage: 1, //初始页
+        pagesize: 10, //    每页的数据
+
+        deleteDialogVisible: false, //控制弹出删除窗口
+        deletingIndex: -1,
+        deletingObject: {},
+
+        methodType: "", //调用方法 Add or Update
+        formTitle: "",
+        dialogFormVisible: false,
+        updatingIndex: -1,
+        updatingObject: {},
+        form: {
+          deptcode: "",
+          deptclass: "",
+          deptname: "",
+          deptcat: "",
+        },
+        formLabelWidth: '120px',
+      };
+
+    },
+
+    mounted: function () {
+      console.log("refresh!!!")
+      this.currentPage = 1;
+      var that = this;
+      this.$axios.post('/api/management/depts', {})
+        .then(function (response) {
+          console.log(response.data);
+          that.depts = response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+
+    methods: {
+      search: function () {
+        this.currentPage = 1;
+        var that = this;
+        this.$axios.post('/api/management/depts', {
+          deptcode: that.deptcode,
+          deptclass: that.deptclass,
+          deptname: that.deptname,
+          deptcat: that.deptcat,
+        })
+          .then(function (response) {
+            console.log(response.data);
+            that.depts = response.data;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      },
+
+      refresh: function () {
+        this.currentPage = 1;
+        var that = this;
+        this.$axios.post('/api/management/depts', {
+          deptcode: "",
+          deptclass: "",
+          deptname: "",
+          deptcat: "",
+        })
+          .then(function (response) {
+            console.log(response.data);
+            that.depts = response.data;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      },
+
+      /*---------------Update Or Add-----------------*/
+      showDialogForm: function (method, index, row) {
+        if (method === "Add") {
+          this.form = {
+            deptcode: "",
+            deptclass: "",
+            deptname: "",
+            deptcat: "",
+          }
+        }
+        if (method === "Update") {
+          this.form = {
+            deptcode: row.deptcode,
+            deptclass: row.deptclass,
+            deptname: row.deptname,
+            deptcat: row.deptcat,
+          }
+        }
+        // this.form = row;
+
+        this.dialogFormVisible = true;
+        this.methodType = method;
+        this.formTitle = method + " Department";
+        this.updatingIndex = index;
+        this.updatingObject = row;
+      },
+
+      onSubmit: function () {
+        if (this.methodType === "Add")
+          this.onSubmitAdd();
+        if (this.methodType === "Update")
+          this.onSubmitUpdate();
+        this.dialogFormVisible = false;
+        this.refresh();
+      },
+
+      onSubmitAdd: function () {
+        var that = this;
+        console.log(that.form);
+        this.$axios.post('/api/management/addDept', {
+          deptcode: that.form.deptcode,
+          deptclass: that.form.deptclass,
+          deptname: that.form.deptname,
+          deptcat: that.form.deptcat,
+        })
+          .then(function (response) {
+            console.log(response.data);
+            that.refresh();
+            // that.depts=response.data;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      },
+
+      onSubmitUpdate: function () {
+        var that = this;
+        console.log(that.form);
+        this.$axios.post('/api/management/updateDept', {
+          deptcode: that.form.deptcode,
+          deptclass: that.form.deptclass,
+          deptname: that.form.deptname,
+          deptcat: that.form.deptcat
+        }).then(function (response) {
+          console.log(response.data);
+          that.refresh();
+          // that.depts=response.data;
+        })
+          .catch(function (error) {
+            console.log(error);
+          });
+      },
+      /*---------------Delete----------------------*/
+      showDeleteDialogForm: function (index, row) {
+        this.deleteDialogVisible = true;
+        this.deletingIndex = index;
+        this.deletingObject = row;
+      },
+
+      onSubmitDelete: function () {
+        var that = this;
+        var params = new URLSearchParams();
+        params.append('deptcode', this.deletingObject.deptcode);
+        this.$axios.post('/api/management/delDept', params)
+          .then(function (response) {
+            // console.log(index.data);
+            /*console.log(that.deptcode);
+            console.log(response.deptcode);
+            that.deptcode = row.deptcode;
+            that.deptcode = response.deptcode;*/
+          }).then(function (response) {
+          that.refresh();
+          // that.depts=response.data;
+        })
+          .catch(function (error) {
+            console.log(error);
+          });
+        this.deleteDialogVisible = false;
+      },
+
+
+      handleSizeChange: function (pagesize) {
+        this.pagesize = pagesize;
+        console.log(pagesize);
+      },
+      handleCurrentChange: function (currentPage) {
+        this.currentPage = currentPage;
+        console.log(currentPage);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
